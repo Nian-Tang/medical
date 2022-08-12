cd19543bdfa13bef6f2a84bd200e098718a9824e
Nian-Tang <110972178+Nian-Tang@users.noreply.github.com>
2022-08-12 16:38:11 +0800

    新增功能：根据id修改密码 测试成功

--- src/main/java/com/medical/controller/UserController.java
+++ src/main/java/com/medical/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.medical.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.org.atool.fluent.mybatis.base.crud.IQuery;
+import cn.org.atool.fluent.mybatis.base.crud.JoinBuilder;
+import com.medical.entity.User;
+import com.medical.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.management.Query;
 
 /**
  * <p>
@@ -16,8 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medical/user")
 public class UserController {
+    @Autowired
+    UserServiceImpl userService;
     /**
      * 修改密码
+     * JiaJieTang
      */
-
+    @PostMapping("updatePassword")
+    public Boolean updatePassword(@RequestBody User user){
+        System.out.println(user);
+        boolean save = userService.updateById(user);
+        return save;
+    }
 }
