089d51a0b85e1ef3349e225fc1640b7925b9c452
@student-wang-rui <2072620686@qq.com>
2022-08-15 14:55:17 +0800

    查询用户病例和问诊记录

--- src/main/java/com/medical/controller/RegisteredController.java
+++ src/main/java/com/medical/controller/RegisteredController.java
@@ -1,9 +1,16 @@
 package com.medical.controller;
 
 
+import com.medical.entity.Registered;
+import com.medical.service.RegisteredService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
