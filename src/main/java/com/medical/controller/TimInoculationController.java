c78a2cde7ce38dbe42f50996d642147d2d746dac
李沅斌 <1140881509@qq.com>
2022-08-16 19:23:49 +0800

    新增 查询我的预约功能

--- src/main/java/com/medical/controller/TimInoculationController.java
+++ src/main/java/com/medical/controller/TimInoculationController.java
@@ -1,6 +1,11 @@
 package com.medical.controller;
 
 
+import com.medical.entity.TimInoculation;
+import com.medical.entity.User;
+import com.medical.mapper.TimInoculationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medical/tim-inoculation")
 public class TimInoculationController {
+    @Autowired
+    TimInoculationMapper timInoculationMapper;
+
+    /**
+     * 我预约的疫苗 lyb
+     */
+    @GetMapping("/selectTiminoculation")
+    public TimInoculation byid(User user){
+        return timInoculationMapper.selectTimInoculation(user.getId());
+    }
 
 }
