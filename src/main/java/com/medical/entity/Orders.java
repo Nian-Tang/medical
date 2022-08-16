c0d860180a9b0d6003a9e4d0b1a3547609930198
傲柒 <2632952978@qq.com>
2022-08-16 08:54:03 +0800

    退款

--- src/main/java/com/medical/entity/Orders.java
+++ src/main/java/com/medical/entity/Orders.java
@@ -1,36 +1,73 @@
 package com.medical.entity;
 
-    import java.io.Serializable;
-    import lombok.Data;
-    import lombok.EqualsAndHashCode;
-    import lombok.experimental.Accessors;
+import java.io.Serializable;
+
+import com.medical.service.OrdersService;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
-* <p>
-    * 订单
-    * </p>
-*
-* @author JiaJieTang
-* @since 2022-08-11
-*/
-    @Data
-        @EqualsAndHashCode(callSuper = false)
-    @Accessors(chain = true)
-    public class Orders implements Serializable {
+ * <p>
+ * 订单
+ * </p>
+ *
+ * @author JiaJieTang
+ * @since 2022-08-11
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Orders implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private int id;
     private Integer uid;
 
-            /**
-            * 订单状态
-            */
+    /**
+     * 订单状态
+     */
     private String orderState;
 
-            /**
-            * 订单编号
-            */
+    /**
+     * 订单编号
+     */
     private Integer orderNumber;
 
+    public String getOrderState() {
+        if (null != orderState)
+            return orderState;
+        String desc = "未知";
+        switch (orderState) {
+            case OrdersService.waitPay:
+                desc = "待付";
+                break;
+            case OrdersService.waitDelivery:
+                desc = "待发";
+                break;
+            case OrdersService.waitConfirm:
+                desc = "待收";
+                break;
+            case OrdersService.waitReview:
+                desc = "等评";
+                break;
+            case OrdersService.finish:
+                desc = "完成";
+                break;
+            case OrdersService.delete:
+                desc = "刪除";
+                break;
+            case OrdersService.backPay:
+                desc = "退款";
+                break;
+            default:
+                desc = "未知";
+        }
+        orderState = desc;
+        return orderState;
+    }
 
+    public void setOrderState(String orderState) {
+        this.orderState = orderState;
+    }
 }
--- src/main/java/com/medical/web/ForeRESTController.java
+++ src/main/java/com/medical/web/ForeRESTController.java
@@ -2,10 +2,10 @@ package com.medical.web;
 
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
-import com.medical.entity.Consulting;
-import com.medical.entity.User;
-import com.medical.entity.Vip;
+import com.medical.entity.*;
+import com.medical.service.OrdersService;
 import com.medical.service.impl.ConsultingServiceImpl;
+import com.medical.service.impl.OrdersServiceImpl;
 import com.medical.service.impl.UserServiceImpl;
 import com.medical.service.impl.VipServiceImpl;
 import com.medical.util.Result;
@@ -24,6 +24,10 @@ public class ForeRESTController {
     VipServiceImpl vipServiceImpl;
     @Autowired
     ConsultingServiceImpl consultingServiceImpl;
+    @Autowired
+    OrdersService ordersService;
+    @Autowired
+    OrdersServiceImpl ordersServiceImpl;
 
     //开通VIP
     @PostMapping("/openvip")
@@ -33,12 +37,12 @@ public class ForeRESTController {
         Vip vip = new Vip();
         Date date1 = vip.getExpireDate();
         Date date2 = new Date();
-        if (user.getVipId() == null  || date1.before(date2)) {//判断时间date1是否在date2之前
+        if (user.getVipId() == null || date1.before(date2)) {//判断时间date1是否在date2之前
             vip.setRegisterDate(new Date());//开始时间
             //开始时间加一个num个月
             Calendar cal = Calendar.getInstance();
             cal.setTime(vip.getRegisterDate());
-            cal.add(Calendar.MONTH,num);
+            cal.add(Calendar.MONTH, num);
 
             vip.setExpireDate(cal.getTime());//结束时间
             vipServiceImpl.save(vip);
@@ -49,10 +53,10 @@ public class ForeRESTController {
             //结束时间加一个num个月
             Calendar cal = Calendar.getInstance();
             cal.setTime(vip.getExpireDate());
-            cal.add(Calendar.MONTH,num);
+            cal.add(Calendar.MONTH, num);
 
             UpdateWrapper<Vip> vipUpdateWrapper = new UpdateWrapper<>();
-            vipUpdateWrapper.eq("id",user.getVipId()).set("expire_date",cal.getTime());
+            vipUpdateWrapper.eq("id", user.getVipId()).set("expire_date", cal.getTime());
             vipServiceImpl.update(vipUpdateWrapper);
 
             return Result.success("续费成功");
@@ -68,5 +72,18 @@ public class ForeRESTController {
         map.put("msg", "查询成功！");
         map.put("date", consultings);
         return map;
+    }
+
+    //退款
+    @GetMapping("/reimburse")
+    public Object reimburse(@RequestParam("oid") int oid, HttpSession httpSession) {
+        User user = (User) httpSession.getAttribute("user");
+        if (user == null)
+            return Result.fail("未登录");
+        Orders orders = ordersServiceImpl.getById(oid);
+        UpdateWrapper<Orders> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("id", orders.getId()).set("order_state", OrdersService.backPay);
+        ordersServiceImpl.update(updateWrapper);
+        return Result.success("退款成功");
     }
 }
