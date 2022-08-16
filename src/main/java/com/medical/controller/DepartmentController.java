eaaa4bbe2e54d342b8739624b75b5699e191d7ab
李沅斌 <1140881509@qq.com>
2022-08-16 19:08:07 +0800

    新增科室查询和查询医院名称地址科室名

--- src/main/java/com/medical/controller/DepartmentController.java
+++ src/main/java/com/medical/controller/DepartmentController.java
@@ -1,9 +1,17 @@
 package com.medical.controller;
 
 
+import com.medical.entity.Department;
+import com.medical.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +24,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medical/department")
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    /**
+     * 科室查询 李豪
+     */
+    @PostMapping("/getDep")
+    public Map<String, Object> get(int id){
+        List<Department> getDepartment= departmentService.getDep(id);
+        Map<String, Object> map=new HashMap<>();
+        map.put("code", "200");
+        map.put("msg", "查询成功");
+        map.put("data", getDepartment);
+        return map;
+    }
+
+    /**
+     * 查询医院名，医院地址，科室名 李豪
+     */
+    @PostMapping("/getks")
+    public Map<String, Object> getks(int hid){
+        List<Department> getDepartment= departmentService.getks(hid);
+        Map<String, Object> map=new HashMap<>();
+        map.put("code", "200");
+        map.put("msg", "查询成功");
+        map.put("data", getDepartment);
+        return map;
+    }
 
 }
--- src/main/java/com/medical/mapper/DepartmentMapper.java
+++ src/main/java/com/medical/mapper/DepartmentMapper.java
@@ -3,6 +3,8 @@ package com.medical.mapper;
 import com.medical.entity.Department;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  * 科室 Mapper 接口
@@ -12,5 +14,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2022-08-11
  */
 public interface DepartmentMapper extends BaseMapper<Department> {
+
+    List<Department> getDep(int id);
+
+    List<Department> getks(int hid);
 
 }
