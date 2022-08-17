214a940c857233cdc1115743660b703ced1d439d
GHB <2635875623@qq.com>
2022-08-17 18:41:47 +0800

    龚海波：医院数量查询

--- src/main/java/com/medical/controller/HospitalController.java
+++ src/main/java/com/medical/controller/HospitalController.java
@@ -1,8 +1,14 @@
 package com.medical.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.medical.config.Result;
+import com.medical.mapper.HospitalMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,11 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
  * 医院 前端控制器
  * </p>
  *
- * @author JiaJieTang
+ * @author GongHaiBo
  * @since 2022-08-11
  */
 @RestController
 @RequestMapping("/medical/hospital")
 public class HospitalController {
 
+    @Autowired
+    HospitalMapper hospitalMapper;
+
+    @GetMapping("/getHospitalCount")
+    public Object getHospitalCount(){
+        QueryWrapper queryWrapper = new QueryWrapper();
+        Integer count = Math.toIntExact(hospitalMapper.selectCount(queryWrapper));
+        return Result.success(count);
+    }
 }
