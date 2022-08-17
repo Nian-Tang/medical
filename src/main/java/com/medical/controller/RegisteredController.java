7051dca44a798224cdab592e39921d5bbbf7ab2c
文林超 <2830837403@qq.com>
2022-08-17 10:08:03 +0800

    挂号查询

--- src/main/java/com/medical/controller/RegisteredController.java
+++ src/main/java/com/medical/controller/RegisteredController.java
@@ -1,8 +1,14 @@
 package com.medical.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.medical.entity.Registered;
+import com.medical.mapper.RegisteredMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medical/registered")
 public class RegisteredController {
+
+    @Autowired
+    RegisteredMapper registeredMapper;
+
+    @GetMapping("/registereds")
+    public Page<Registered> registered(@RequestParam(value = "start",defaultValue = "1") int start,
+                                       @RequestParam(value = "size" ,defaultValue = "10") int size){
+        Page<Registered> registeredPage = registeredMapper.selectPage(new Page<>(start,size),null);
+        return registeredPage;
+    }
 
 }
