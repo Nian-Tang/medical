13c65bbe0a9480dc0fe38ebfdec0af4bc9d83be3
Nian-Tang <110972178+Nian-Tang@users.noreply.github.com>
2022-08-17 09:32:18 +0800

    对查询关注的结果进行分页 对查询挂号预约的结果进行分页 新增分页工具类

--- src/main/java/com/medical/controller/ConcernController.java
+++ src/main/java/com/medical/controller/ConcernController.java
@@ -1,16 +1,21 @@
 package com.medical.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.medical.entity.User;
 import com.medical.service.impl.ConcernServiceImpl;
+import com.medical.util.PageList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -33,9 +38,12 @@ public class ConcernController {
      * @return
      */
     @GetMapping("selectConcern")
-    public List<User> selectConcern(HttpSession session){
+    public Map<String, Object> selectConcern(HttpSession session, @RequestParam("pageSize") Integer pageSize, @RequestParam("pageIndex") Integer pageIndex){
         User user=(User)session.getAttribute("user");
         List<User> users = concernService.selectConcern(user.getId());
-        return users;
+        Page<Object> pageList = PageList.pageList(users, pageSize, pageIndex);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("success",pageList);
+        return map;
     }
 }
--- src/main/java/com/medical/controller/RegisteredController.java
+++ src/main/java/com/medical/controller/RegisteredController.java
@@ -1,17 +1,22 @@
 package com.medical.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.medical.entity.Registered;
 import com.medical.entity.User;
 import com.medical.service.impl.RegisteredServiceImpl;
+import com.medical.util.PageList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -33,9 +38,12 @@ public class RegisteredController {
      * @return
      */
     @GetMapping("selRegistered")
-    public List<Registered> selRegistered(HttpSession session){
+    public Map<String, Object> selRegistered(HttpSession session, @RequestParam Integer pageIndex, @RequestParam Integer pageSize){
         User user = (User) session.getAttribute("user");
         List<Registered> registereds = registeredService.selRegistered(user.getId());
-        return registereds;
+        Page<Object> pageList = PageList.pageList(registereds, pageSize, pageIndex);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("success",pageList);
+        return map;
     }
 }
