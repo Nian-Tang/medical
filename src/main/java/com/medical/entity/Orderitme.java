4a6862d6459f1d18b081084651f57f4618481325
草包 <203557550@qq.com>
2022-08-18 09:53:43 +0800

    默认变更列表

--- src/main/java/com/medical/entity/Orderitme.java
+++ src/main/java/com/medical/entity/Orderitme.java
@@ -1,46 +1,59 @@
 package com.medical.entity;
 
-    import java.io.Serializable;
-    import lombok.Data;
-    import lombok.EqualsAndHashCode;
-    import lombok.experimental.Accessors;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
-* <p>
-    * 订单项
-    * </p>
-*
-* @author JiaJieTang
-* @since 2022-08-11
-*/
-    @Data
-        @EqualsAndHashCode(callSuper = false)
-    @Accessors(chain = true)
-    public class Orderitme implements Serializable {
+ * <p>
+ * 订单项
+ * </p>
+ *
+ * @author JiaJieTang
+ * @since 2022-08-11
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Orderitme implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
 
     private Integer id;
-            /**
-            * 药品id
-            */
+    /**
+     * 药品id
+     */
+    Integer drid;
+
+    @TableField(exist = false)
     private Drug drug;
 
-            /**
-            * 用户id
-            */
-    private User user;
+    /**
+     * 用户id
+     */
+    Integer uid;
 
-            /**
-            * 购买数量
-            */
+    @TableField
+    private User user;
+    /**
+     * 购买数量
+     */
     private Integer number;
 
-            /**
-            * 订单id
-            */
+    /**
+     * 订单id
+     */
+    private Integer oid;
+
+    @TableField(exist = false)
     private Orders orders;
+
+
+
 
 
 }
