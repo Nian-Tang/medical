194246de119cf52377f3613f2bb308c3f6f3c76f
屎倒淋头还搅便 <2723184685@qq.com>
2022-08-19 14:42:27 +0800

    新增添加疫苗功能

--- src/main/java/com/medical/entity/Vaccine.java
+++ src/main/java/com/medical/entity/Vaccine.java
@@ -3,6 +3,10 @@ package com.medical.entity;
     import java.time.LocalDateTime;
     import java.time.LocalTime;
     import java.io.Serializable;
+
+    import com.baomidou.mybatisplus.annotation.IdType;
+    import com.baomidou.mybatisplus.annotation.TableId;
+    import com.fasterxml.jackson.annotation.JsonFormat;
     import lombok.Data;
     import lombok.EqualsAndHashCode;
     import lombok.experimental.Accessors;
@@ -21,7 +25,7 @@ package com.medical.entity;
     public class Vaccine implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
 
             /**
@@ -37,6 +41,7 @@ package com.medical.entity;
             /**
             * 生产日期
             */
+            @JsonFormat( pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime vaccineDate;
 
             /**
@@ -57,21 +62,25 @@ package com.medical.entity;
             /**
             * 上午接种时间
             */
+            @JsonFormat( pattern = "HH:mm:ss")
     private LocalTime amLnoculationDate;
 
             /**
             * 上午结束接种时间
             */
+            @JsonFormat( pattern = "HH:mm:ss")
     private LocalTime amOverDate;
 
             /**
             * 下午接种时间
             */
+            @JsonFormat( pattern = "HH:mm:ss")
     private LocalTime pmLnoculationDate;
 
             /**
             * 下午结束接种时间
             */
+            @JsonFormat( pattern = "HH:mm:ss")
     private LocalTime pmOverDate;
 
 
